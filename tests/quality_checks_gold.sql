cd87115ba59917ce55768f90827dc70fd246ebce
RAVI RANJAN KUMAR <donravi512@gmail.com>
2025-05-27 16:15:46 +0530

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,1 +1,15 @@
+/*
+===================================================================================================
+Quality Checks
+===================================================================================================
+Script Purpose:
+  This script performs quality checks to validate the integrity, consistency, and accuracy of the Gold Layer. These checks ensure:
+  - Uniqueness of surrogate keys in dimension tables.
+  - Referential integrity between fact and dimension tables.
+  - Validation of relationships in the data model for analytical purposes.
+Usase Notes:
+  - Run these checks after data loading silver layer.
+  - Investigate and resolve any discrepancies found during the checks.
+===================================================================================================
+*/
 
